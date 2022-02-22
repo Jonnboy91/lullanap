d217959f9736551ed91a2ef7b2fd948be1c17998
jaanikaukonen <kaukonen.jaani@gmail.com>
2022-02-22 18:49:39 +0200

    Add colors, types and background images.
    Add colors for dark and light themes, add fonts and use them as default, setting theme and background image based on sys prefs.

--- app/src/main/java/com/jonnesten/lullanap/MainActivity.kt
+++ app/src/main/java/com/jonnesten/lullanap/MainActivity.kt
@@ -3,6 +3,8 @@ package com.jonnesten.lullanap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -10,6 +12,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.jonnesten.lullanap.bottomnav.BottomNavigation
@@ -20,19 +25,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreenView()
+            LullaNapTheme {
+                MainScreenView()
+            }
         }
     }
 }
 
 @Composable
 fun MainScreenView(){
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavigation(navController = navController) }
-    ) {
+    Box {
+        val isLightTheme = MaterialTheme.colors.isLight
+        Image(
+            painterResource(if (isLightTheme) R.drawable.lulla_bg_light else R.drawable.lulla_bg_dark),
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = "Background Image",
+            contentScale = ContentScale.FillBounds
+        )
+        val navController = rememberNavController()
+        Scaffold(
+            backgroundColor = Color.Transparent,
+            bottomBar = { BottomNavigation(navController = navController) }
+        ) {
 
-        NavigationGraph(navController = navController)
+            NavigationGraph(navController = navController)
+        }
     }
 }
 
