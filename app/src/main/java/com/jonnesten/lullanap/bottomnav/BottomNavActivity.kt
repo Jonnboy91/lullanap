3db3395cc12558dd6c0582abf9efa1e1f09519b7
jaanikaukonen <kaukonen.jaani@gmail.com>
2022-03-09 14:18:53 +0200

    Bottom navigation bar styling

--- app/src/main/java/com/jonnesten/lullanap/bottomnav/BottomNavActivity.kt
+++ app/src/main/java/com/jonnesten/lullanap/bottomnav/BottomNavActivity.kt
@@ -1,11 +1,13 @@
 package com.jonnesten.lullanap.bottomnav
 
 import android.content.SharedPreferences
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -24,19 +26,29 @@ fun BottomNavigation(navController: NavController) {
         BottomNavItem.Settings,
     )
     BottomNavigation(
-        backgroundColor = Color.White,
-        contentColor = Color.Black
+        backgroundColor = MaterialTheme.colors.primary,
+        elevation = 0.dp
     ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             BottomNavigationItem(
-                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
-                label = { Text(text = item.title,
-                    fontSize = 9.sp) },
-                selectedContentColor = Color.Black,
-                unselectedContentColor = Color.Black.copy(0.4f),
-                alwaysShowLabel = true,
+                icon = {
+                    Icon(
+                        item.icon,
+                        contentDescription = item.title,
+                        modifier = Modifier.size(29.dp)
+                    )
+                },
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                },
+                selectedContentColor = MaterialTheme.colors.secondaryVariant,
+                unselectedContentColor = MaterialTheme.colors.onPrimary.copy(0.6f),
                 selected = currentRoute == item.screen_route,
                 onClick = {
                     navController.navigate(item.screen_route) {
